09484c9304ce34ef38fd8d7ae7a839e7c84e69ec
cecilialindeborg <cecilia.lindeborg@gmail.com>
2026-05-12 10:48:31 +0200

    fix

---
+++ a4/task1.py
@@ -0,0 +1,68 @@
+from sklearn.datasets import fetch_covtype
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
+import ray
+from ray import tune
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+
+
+X, y = fetch_covtype(return_X_y=True)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+rf = RandomForestClassifier()
+params = rf.get_params()
+print("Parameters:", params)
+
+rf.fit(X_train, y_train)
+y_pred = rf.predict(X_test)
+
+acc = accuracy_score(y_test, y_pred)
+
+print("Accuracy: ", acc)
+
+scores = cross_val_score(
+    rf, X_train, y_train, cv=3
+)
+
+print("CV Scores:", scores)
+print("Mean CV Score:", scores.mean())
+
+ray.shutdown()
+ray.init(address="auto")
+
+def train_rf(config):
+
+    model = RandomForestClassifier(
+        max_depth=config["max_depth"],
+        n_estimators=config["n_estimators"],
+        ccp_alpha=config["ccp_alpha"],
+    )
+
+    scores = cross_val_score(
+        model, X_train, y_train, cv=3
+    )
+
+    tune.report(cv_score=scores.mean())
+
+search_space = {
+    "max_depth": tune.grid_search([10, 20]),
+    "n_estimators": tune.grid_search([50, 100]),
+    "ccp_alpha": tune.grid_search([0.0])
+}
+
+tuner = tune.Tuner(
+    train_rf,
+    param_space=search_space
+)
+
+results = tuner.fit()
+best_result = results.get_best_result(metric="cv_score", mode="max")
+
+print("Best config", best_result.config)
+print(best_result.metrics["cv_score"])
