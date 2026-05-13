2b38c8120e6cfa5025dd3b5b0d13208cb0bfd5ec
cecilialindeborg <cecilia.lindeborg@gmail.com>
2026-05-13 13:34:41 +0200

    test

--- a4/task1.py
+++ a4/task1.py
@@ -15,7 +15,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-rf = RandomForestClassifier()
+rf = RandomForestClassifier(random_state=42)
 params = rf.get_params()
 print("Parameters:", params)
 
@@ -33,42 +33,42 @@ scores = cross_val_score(
 print("CV Scores:", scores)
 print("Mean CV Score:", scores.mean())
 
-ray.shutdown()
-ray.init(address="auto")
+# ray.shutdown()
+# ray.init(address="auto")
 
-X_train_ref = ray.put(X_train)
-y_train_ref = ray.put(y_train)
+# X_train_ref = ray.put(X_train)
+# y_train_ref = ray.put(y_train)
 
-def train_rf(config):
-    X_train_local = ray.get(X_train_ref)
-    y_train_local = ray.get(y_train_ref)
+# def train_rf(config):
+#     X_train_local = ray.get(X_train_ref)
+#     y_train_local = ray.get(y_train_ref)
 
-    model = RandomForestClassifier(
-        max_depth=config["max_depth"],
-        n_estimators=config["n_estimators"],
-        ccp_alpha=config["ccp_alpha"],
-        n_jobs=1
-    )
+#     model = RandomForestClassifier(
+#         max_depth=config["max_depth"],
+#         n_estimators=config["n_estimators"],
+#         ccp_alpha=config["ccp_alpha"],
+#         n_jobs=1
+#     )
 
-    scores = cross_val_score(
-        model, X_train_local, y_train_local, cv=3, n_jobs=1
-    )
+#     scores = cross_val_score(
+#         model, X_train_local, y_train_local, cv=3, n_jobs=1
+#     )
 
-    tune.report(cv_score=scores.mean())
+#     tune.report(cv_score=scores.mean())
 
-search_space = {
-    "max_depth": tune.grid_search([10, 20]),
-    "n_estimators": tune.grid_search([50, 100]),
-    "ccp_alpha": tune.grid_search([0.0])
-}
+# search_space = {
+#     "max_depth": tune.grid_search([10, 20]),
+#     "n_estimators": tune.grid_search([50, 100]),
+#     "ccp_alpha": tune.grid_search([0.0])
+# }
 
-tuner = tune.Tuner(
-    train_rf,
-    param_space=search_space
-)
+# tuner = tune.Tuner(
+#     train_rf,
+#     param_space=search_space
+# )
 
-results = tuner.fit()
-best_result = results.get_best_result(metric="cv_score", mode="max")
+# results = tuner.fit()
+# best_result = results.get_best_result(metric="cv_score", mode="max")
 
-print("Best config", best_result.config)
-print(best_result.metrics["cv_score"])
+# print("Best config", best_result.config)
+# print(best_result.metrics["cv_score"])
